dd82f11c2568c03ff0b50ecf250a3a6a84de0b77
HI0516 <wangsun7@naver.com>
2023-05-18 16:51:49 +0900

    feat: Jwt Service 추가

--- src/main/java/com/ssafy/enjoytrip/util/JwtInterceptor.java
+++ src/main/java/com/ssafy/enjoytrip/util/JwtInterceptor.java
@@ -1,31 +1,32 @@
-package com.ssafy.enjoytrip.util;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Component
-public class JwtInterceptor implements HandlerInterceptor {
-
-    private JwtUtil jwtUtil;
-    private static final String HEADER_AUTH = "auth-token";
-
-    @Autowired
-    public JwtInterceptor(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        final String token = request.getHeader(HEADER_AUTH);
-
-        if(token != null && jwtUtil.checkToken(token)){
-            return true;
-        }
-
-        throw new RuntimeException();
-    }
-}
+//package com.ssafy.enjoytrip.util;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Component
+//public class JwtInterceptor implements HandlerInterceptor {
+//
+//    private
+//    JwtUtil jwtUtil;
+//    private static final String HEADER_AUTH = "auth-token";
+//
+//    @Autowired
+//    public JwtInterceptor(JwtUtil jwtUtil) {
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        final String token = request.getHeader(HEADER_AUTH);
+////
+////        if(token != null && jwtUtil.checkToken(token)){
+////            return true;
+////        }
+//
+//        throw new RuntimeException();
+//    }
+//}
